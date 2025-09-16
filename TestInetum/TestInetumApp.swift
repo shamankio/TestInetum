e070f79d32ca7ff8540124c5974f667411ceddb6
Oleksandr <rustanovych@gmail.com>
2025-09-16 21:09:32 +0200

    Initial commit

---
+++ TestInetum/TestInetumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestInetumApp.swift
+//  TestInetum
+//
+//  Created by Oleksandr Rustanovych on 16.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TestInetumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestInetumTests/TestInetumTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestInetumTests.swift
+//  TestInetumTests
+//
+//  Created by Oleksandr Rustanovych on 16.09.2025.
+//
+
+import Testing
+@testable import TestInetum
+
+struct TestInetumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
