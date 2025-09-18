f895ed00d93ef70a032a3df769d3e0253f6cb684
Oleksandr <rustanovych@gmail.com>
2025-09-18 08:43:55 +0200

    tweak fonts

--- TestInetum/presentation/salesmans/SalesmanRowView.swift
+++ TestInetum/presentation/salesmans/SalesmanRowView.swift
@@ -11,45 +11,47 @@ struct SalesmanRowView: View {
     let salesman: Salesman
     let isExpanded: Bool
     let onToggleExpansion: () -> Void
-       
-       var body: some View {
-           VStack(alignment: .leading, spacing: 0) {
-               HStack(spacing: 8) {
-                   Circle().stroke(Color.divider,lineWidth: 1)
-                       .fill(Color.iconBackground)
-                       .frame(width: 42, height: 42)
-                    
-        
-                       .overlay(
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 8) {
+                Circle().stroke(Color.divider, lineWidth: 1)
+                    .fill(Color.iconBackground)
+                    .frame(width: 42, height: 42)
+
+                    .overlay(
                         Text(String(salesman.name.prefix(1)))
                             .foregroundColor(.black)
-                               .font(.system(size: 17, weight: .semibold))
-                       )
-                   
-                   VStack(alignment: .leading, spacing: 4) {
-                       Text(salesman.name)
-                           .font(.system(size: 17, weight: .semibold))
-                       
-                       if isExpanded {
-                           HStack {
-                                   Text(salesman.areas.joined(separator: ", "))
-                                       .font(.system(size: 14, weight: .regular))
-                                       .foregroundColor(Color("ColorGray400"))
-                           }
-                       }
-                   }
-                   
-                   Spacer()
-                   
-                   Button(action: onToggleExpansion) {
-                       Image(systemName: isExpanded ? "chevron.down" : "chevron.right")
-                           .foregroundColor(Color("ColorGray400"))
-                           .imageScale(.small)
-                   }.padding(.trailing,16)
-               }
-               .padding(.vertical, 8)
-           }.frame(width: .infinity, height: 68)
-       }
+                            .font(.title2)
+                    )
+
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(salesman.name)
+                        .font(.headline)
+
+                    if isExpanded {
+                        HStack {
+                            Text(salesman.areas.joined(separator: ", "))
+                                .font(.subheadline)
+                                .foregroundColor(Color.colorGray400)
+                        }
+                    }
+                }
+
+                Spacer()
+
+                Button(action: onToggleExpansion) {
+                    Image(
+                        systemName: isExpanded
+                            ? "chevron.down" : "chevron.right"
+                    )
+                    .foregroundColor(Color.colorGray400)
+                    .imageScale(.small)
+                }.padding(.trailing, 16)
+            }
+            .padding(.vertical, 8)
+        }.frame(width: .infinity, height: 68)
+    }
 }
 
 #Preview("normal") {
@@ -57,7 +59,8 @@ struct SalesmanRowView: View {
         salesman: Salesman(name: "Artem Anderson", areas: ["76133"]),
         isExpanded: false,
         onToggleExpansion: {
-        })
+        }
+    )
     .padding()
 }
 #Preview("expanded") {
@@ -65,6 +68,7 @@ struct SalesmanRowView: View {
         salesman: Salesman(name: "Artem Anderson", areas: ["76133", "76132"]),
         isExpanded: true,
         onToggleExpansion: {
-        })
+        }
+    )
     .padding()
 }
